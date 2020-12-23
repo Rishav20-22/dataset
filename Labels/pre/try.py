a05ea12441852bd0fa8eb856a3f3003405e17c37
Rishav Kumar <rishavk@Rishavs-MacBook-Pro.local>
2020-12-22 19:01:46 -0500

    Intial Commit

---
+++ Labels/pre/try.py
@@ -0,0 +1,17 @@
+import os
+def createTrainvalTxt(baseDirDataSet):
+    buffer = ''
+    baseDir = baseDirDataSet+'\Images'
+    for filename in os.listdir(baseDir):
+        filenameOnly, file_extension = os.path.splitext(filename)
+        # print (file_extension)
+        s = 'Images/'+filenameOnly+'.jpg'+' '+'Labels/'+filenameOnly+'.xml\n'
+        print (repr(s))
+        img_file, anno = s.strip("\n").split(" ")
+        print(repr(img_file), repr(anno))
+        buffer+=s
+    with open(baseDirDataSet+'\\Structure\\trainval.txt', 'w') as file:
+        file.write(buffer)  
+    print('Done')   
+#Usage
+createTrainvalTxt('/Users/rishavk/Desktop/dataset/Dataset')
