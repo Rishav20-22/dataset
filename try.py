eb72dc35c01101588d09ca817dcb7c2f87b6b1d6
Rishav20-22 <rachana.rishav@gmail.com>
2021-01-13 13:15:32 -0500

    first commit

--- try.py
+++ try.py
@@ -1,7 +1,9 @@
 import os
+
+
 def createTrainvalTxt(baseDirDataSet):
     buffer = ''
-    baseDir = baseDirDataSet+'\Images'
+    baseDir = baseDirDataSet+'/Images'
     for filename in os.listdir(baseDir):
         filenameOnly, file_extension = os.path.splitext(filename)
         # print (file_extension)
@@ -10,8 +12,8 @@ def createTrainvalTxt(baseDirDataSet):
         img_file, anno = s.strip("\n").split(" ")
         print(repr(img_file), repr(anno))
         buffer+=s
-    with open(baseDirDataSet+'\\Structure\\trainval.txt', 'w') as file:
+    with open(baseDirDataSet+'/Structure/trainval.txt', 'w') as file:
         file.write(buffer)  
     print('Done')   
 #Usage
-createTrainvalTxt('/Users/rishavk/Desktop/dataset/Dataset')
+createTrainvalTxt('/home/rishav/dataset/Dataset-ev3-tracking')
